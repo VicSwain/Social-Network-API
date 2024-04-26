a4ce55f3b8411139612169399c35b901f5955dca
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-04-26 18:47:12 -0500

    updated User and Thought model; User model has getter for friendCount and Thought model has getter for reaction count

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: { String, required: true }, 
+        email: { String, required: true }, 
+        thoughts : [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            },
+        ],
+        friends: [Friend],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+// thinking there is a need for a friend count virtual here
+userSchema
+.virtual('getFriends')
+// Getter
+.get(function () {
+    return this.friends.length;
+});
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
