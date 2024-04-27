ecec8f9c9065c777409be5fcadadaecaa4a34ce5
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-04-27 11:18:49 -0500

    adjusted models for AC

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,33 @@
 const { Schema, model } = require('mongoose');
-const reactionSchema = require('./Reaction');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String, 
+            required: true,
+            minlength: 1, 
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date, 
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false 
+    }
+);
 
 const thoughtSchema = new Schema(
     {
