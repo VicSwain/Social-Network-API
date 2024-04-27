43dcee06d1d2a4e889ab276f928cd0a4a6253489
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-04-26 19:05:45 -0500

    added Reaction.js and Friend.js with schema; added index.js to models;

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String, 
+            required: true, 
+            maxlength: 500,
+        },
+        createdAt: {
+            type: Date, 
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false 
+    }
+);
+
+
+module.exports = reactionSchema;
