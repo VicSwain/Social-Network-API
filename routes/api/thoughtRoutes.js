76346e8d57dcda8e86fe608d104a63b73605b530
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-04-28 11:11:27 -0500

    added reaction post/delete route to thoughtRoutes.js; added functionality for post/delete reaction in thoughtController.js;Validation error or createReaction

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,6 +6,8 @@ const {
     createThought,
     updateThought, 
     deleteThought,
+    createReaction,
+    deleteReaction,
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
@@ -16,5 +18,10 @@ router.route('/:thoughtId')
 .get(getSingleThought)
 .put(updateThought)
 .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+    .post(createReaction) 
+    .delete(deleteReaction); 
 
 module.exports = router;
